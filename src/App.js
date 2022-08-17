c40078c6d7d755219051659877d733206eee67cc
chendianbuji <chendianbuji@gmail.com>
2022-08-17 09:33:15 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { s4, s6, cast } from './core.js';
+import { Box, Button, Center, Flex, Stack, HStack, VStack } from "@chakra-ui/react";
+import { ChakraProvider } from '@chakra-ui/react';
+import { GiCrossedSwords } from "react-icons/gi";
+
+console.log(cast([s6, s6]));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ChakraProvider>
+          <HStack h="100vh">
+            <Box>
+              <Button leftIcon={<GiCrossedSwords/>} colorScheme='pink'>挑战</Button>
+            </Box>
+          </HStack>
+        </ChakraProvider>
+    );
 }
 
 export default App;
---
+++ src/core.js
@@ -0,0 +1,97 @@
+
+function makeDice(sides){
+    return function() {
+        return Math.floor(Math.random() * sides) + 1;
+    };
+}
+
+export { makeDice };
+
+function s4() {
+    let d = makeDice(4);
+    return d();
+}
+
+export { s4 };
+
+function s6() {
+    let d = makeDice(6);
+    return d();
+}
+
+export { s6 };
+
+function s8() {
+    let d = makeDice(8);
+    return d();
+}
+
+export { s8 };
+
+function s10() {
+    let d = makeDice(10);
+    return d();
+}
+
+export { s10 };
+
+function s12() {
+    let d = makeDice(12);
+    return d();
+}
+
+export { s12 };
+
+function s20() {
+    let d = makeDice(20);
+    return d();
+}
+
+export { s20 };
+
+function cast(dices) {
+    return dices.map(dice => dice()).reduce((acc, cur) => acc + cur, 0);
+}
+
+export { cast };
+
+let events = {
+    blessing: "祝福",
+    lucky: "幸运",
+    armor: "护甲提升"
+};
+
+const Battle = Object.freeze({
+    Win: Symbol("win"),
+    Lose: Symbol("lose"),
+    Injury: Symbol("injury"),
+    Lucky: Symbol("lucky"),
+    Bless: Symbol("bless")
+});
+
+const monster1 = {
+    counter: 6,
+    injury: 7,
+    lucky: 8,
+    bless: 9,
+    defeat: 10
+};
+
+export { monster1 };
+
+function battle(mon, dices) {
+    let total = cast(dices);
+    if (total <= mon.counter) {
+        return Battle.Lose;
+    } else if (total <= mon.injury) {
+        return Battle.Injury;
+    } else if (total <= mon.lucky) {
+        return Battle.Lucky;
+    } else if (total <= mon.bless) {
+        return Battle.Bless;
+    } else {
+        return Battle.Win;
+    }
+}
+
+export { battle };
